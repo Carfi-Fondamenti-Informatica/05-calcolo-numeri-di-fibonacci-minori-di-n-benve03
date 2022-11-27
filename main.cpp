c02eca56abb962ccc25e80477f5f40eb38125bdb
benve03 <117894307+benve03@users.noreply.github.com>
2022-11-27 23:01:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() { 
+   int n=0, a=1, b=1, c=0;
+    cin >> n;
+    cout << a << endl;
+    cout << b << endl;
+    for (int i=0; i<=n; i++){
+        c=a+b;
+        a=b;
+        b=c;
+        cout <<c<< endl;
+    }
    return 0;
 }
